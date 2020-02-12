369d4bc49c4a1faa5fea9a5958a48d167304dd71
Liren Tu <tuliren@gmail.com>
2020-02-12 00:43:32 -0800

    Updates

---
+++ precache-manifest.c931132b6505840021ec46ef76303c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ee5920b55c98edd90890a2c14412fb",
+    "url": "/dinner/index.html"
+  },
+  {
+    "revision": "d48fe28a4ed5ecfde455",
+    "url": "/dinner/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "224f2bbcf3a362b6d61c",
+    "url": "/dinner/static/js/2.ba0bc400.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/dinner/static/js/2.ba0bc400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48fe28a4ed5ecfde455",
+    "url": "/dinner/static/js/main.87e9e887.chunk.js"
+  },
+  {
+    "revision": "f56004303651543d798a",
+    "url": "/dinner/static/js/runtime-main.b322ae3b.js"
+  }
+]);
